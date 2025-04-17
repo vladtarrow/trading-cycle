e80179ce09934e15768525afbb159fa55339ecef
Vladislav Tarasenko <vladislav.Tarasenko@yessoftware.com>
2025-04-17 03:02:44 +0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'handlers/**/*.ts',
+    '!**/node_modules/**',
+    '!**/dist/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
